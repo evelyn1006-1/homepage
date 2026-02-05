8e1b66f471d2c14f15ef8369729088ea8d4d2e6e
✨Princess Evelyn✨🧡 <249149451+evelyn1006-1@users.noreply.github.com>
2026-02-05 19:07:26 +0100

    Added error pages

--- app.py
+++ app.py
@@ -79,4 +79,24 @@ def create_app():
     def cat():
         return render_template("cat.html")
 
+    @app.get("/forbidden")
+    def test_403():
+        abort(403)
+
+    @app.get("/error")
+    def test_500():
+        abort(500)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template("403.html"), 403
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template("404.html"), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return render_template("500.html"), 500
+
     return app
